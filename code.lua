200fac3d4d734076ec05c2fdfaac821f9c4c0723
Bulo <BuloZB@users.noreply.github.com>
2019-07-21 10:17:07 +0200

    cleanup
    cleanup

--- code.lua
+++ code.lua
@@ -1,18 +1,12 @@
---update toc for patch 8.2
---- This file is loaded from "BuloUI V4.toc"
 MainMenuBarArtFrame:ClearAllPoints()
 MainMenuBarArtFrame:SetPoint("CENTER",2,2)
 MainMenuBarArtFrame.SetPoint = function() end
 MainMenuBarArtFrame:SetScale(1.16)
- --possibli action clear all points
+ 
 ActionButton1:ClearAllPoints()
 ActionButton1:SetPoint("cENTER",-233,-2)
 ActionButton1.SetPoint = function() end
---buttons for edit
---MultiBarBottomRightButton7:ClearAllPoints()
---MultiBarBottomRightButton7:SetPoint("cENTER",-650,41)
---MultiBarBottomRightButton7.SetPoint = function() end
-
+ 
 MultiBarBottomRightButton7:ClearAllPoints()
 MultiBarBottomRightButton7:SetPoint("cENTER",-650,41)
 MultiBarBottomRightButton7.SetPoint = function() end
@@ -64,16 +58,16 @@ TargetFrameSpellBar:ClearAllPoints();TargetFrameSpellBar:SetPoint("CENTER", UIPa
 FocusFrameSpellBar:SetScale(1.88)
  
 FocusFrameSpellBar:ClearAllPoints();FocusFrameSpellBar:SetPoint("CENTER", UIParent, "CENTER", -100, -110);FocusFrameSpellBar.SetPoint = function() end
- --cvar 0 1 / 01
+ 
 SetCVar('BreakUpLargeNumbers', 0)
- --cvar 0 /1
+ 
 PetActionBarFrame:ClearAllPoints()PetActionBarFrame:SetScale(1)PetActionBarFrame:SetPoint("RIGHT", UIParent, "CENTER", 290, -255)PetActionBarFrame.SetPoint=function()end
 local b=HonorWatchBar b:UnregisterAllEvents() b:Hide() b.Show=b.Hide
 local b=ArtifactWatchBar b:UnregisterAllEvents() b:Hide() b.Show=b.Hide
 SetCVar("nameplateOccludedAlphaMult",1)
  
 local t="Interface\\TargetingFrame\\UI-TargetingFrame-rare"PlayerFrameTexture:SetTexture(t)hooksecurefunc("TargetFrame_Update",function(f)if UnitIsPlayer(f.unit)then _G[f:GetName().."TextureFrameTexture"]:SetTexture(t)end end)
- --funciton sp is fot debuffframes
+ 
 function sp(f,i) tr="TOPRIGHT";f2=f.debuffFrames;s=f2[1]:GetWidth();f3=f2[i];f3:SetSize(s,s);f3:ClearAllPoints();if i>6 then f3:SetPoint("BOTTOMRIGHT",f2[i-3],tr,0,0) else f3:SetPoint(tr,f2[1],tr,-(s*(i-3)),0) end end
 function CBF(f,i) bf=CreateFrame("Button",f:GetName().."Debuff"..i,f,"CompactDebuffTemplate");bf.baseSize=22;bf:SetSize(f.buffFrames[1]:GetSize()) end;function mv(f) for i=4,12 do sp(f,i) end end
 function mv3(f) CompactUnitFrame_SetMaxDebuffs(f,12); if not f.debuffFrames[4] then for i=4,12 do CBF(f,i) end end mv(f) end;hooksecurefunc("CompactUnitFrame_UpdateDebuffs",function(f) if f:GetName():match("^Compact") then mv3(f) end end);
